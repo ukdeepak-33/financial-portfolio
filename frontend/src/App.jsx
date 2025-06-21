713d03b46b60bd20f594d83572784336bce974c6
ukdeepak-33 <ukdeepak09@gmail.com>
2025-06-20 19:31:11 -0500

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>AI Portfolio Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
